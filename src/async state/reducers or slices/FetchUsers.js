502ee15854606bb05a87961b0461d71527262c02
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-07-24 00:06:37 +0530

    Added error handling

--- src/async state/reducers or slices/FetchUsers.js
+++ src/async state/reducers or slices/FetchUsers.js
@@ -32,8 +32,13 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
-  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
-  return res.data.map((user) => user); //return value of each callback of map is added to an array which is finally returned by the map function
+  try{
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    return res.data.map((user) => user); //return value of each callback of map is added to an array which is finally returned by the map function
+  }catch (err) {
+    console.log(err)
+    return rejectWithValue(err)
+  }
 });
 const userSlice = createSlice({
   name: "users",
